ce9c2ab2a7a20925de991a0408516a7370a03cac
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2025-10-16 17:09:37 +0200

    feat: add Actix-web dependency and implement a basic health check endpoint
    - Updated Cargo.toml to include actix-web version 4 as a dependency.
    - Modified main.rs to set up an Actix-web server with a "/health" endpoint that returns "ok".

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("tcgpi-service-mtg");
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    "ok"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
